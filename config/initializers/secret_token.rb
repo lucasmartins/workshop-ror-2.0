35b7171ee994369b0f166c40aee1605dafaa019b
Lucas Neves Martins <snowniak@gmail.com>
2012-07-31 02:25:03 -0300

    rails new correios

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Correios::Application.config.secret_token = '3fad2c484b85c51bea6db715c1eea72e169d41fd56ff77ab74f676e9ad305ad54aab134d290a94db876f7b03100c026f07a6bfea87d2d05b7c8ea809aaad75e8'
