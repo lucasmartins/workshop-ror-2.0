bc471cf13db31df5cfbe6c3f0b839298428a5fcc
Lucas Neves Martins <snowniak@gmail.com>
2012-07-31 04:22:49 -0300

    ajustando o CRUD de entrega

--- app/models/entrega.rb
+++ app/models/entrega.rb
@@ -1,4 +1,6 @@
 class Entrega < ActiveRecord::Base
   belongs_to :selo
-  attr_accessible :conteudo, :destinatario, :entrege, :remetente
+  attr_accessible :conteudo, :destinatario, :entrege, :remetente, :selo_id
+
+  validates :destinatario, :remetente, :selo, :presence => true
 end
